8ba6732aa6afb673afce653c11ffb26c5afbc434
diaas14 <diya.diyu14@gmail.com>
2023-03-22 16:37:53 +0530

    Added basic code

---
+++ comments/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  const comments = commentsByPostId[req.params.id] || Array();
+  res.send(comments);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || Array();
+  comments.push({ id, content });
+  commentsByPostId[req.params.id] = comments;
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log("Comments service running on port 4001");
+});
